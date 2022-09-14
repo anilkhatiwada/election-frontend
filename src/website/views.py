390671ecf50d0470a24986991ea830cff866d6ca
Anil Khatiwada <anilkhatiwada@gmail.com>
2022-09-15 00:19:48 +0545

    code changed

--- src/website/views.py
+++ src/website/views.py
@@ -3,12 +3,35 @@ from django.views.generic import View
 import requests
 import json
 
+url = "https://f058-103-163-182-45.in.ngrok.io/api/1/"
+
 
 class HomePageView(View):
     def get(self, request, *args, **kwargs):
         data = requests.get(
-            'https://f058-103-163-182-45.in.ngrok.io/api/1/home').json()
+            url+"home").json()
         return render(request, 'website/index.html', data)
+
+
+class NewsPageView(View):
+    def get(self, request, *args, **kwargs):
+        data = requests.get(
+            url+"news").json()
+        return render(request, 'website/news.html', data)
+
+
+class BlogsPageView(View):
+    def get(self, request, *args, **kwargs):
+        data = requests.get(
+            url+"blog").json()
+        return render(request, 'website/blogs.html', data)
+
+
+class BlogPageView(View):
+    def get(self, request, *args, **kwargs):
+        data = requests.get(
+            url+"blog/"+str(kwargs["id"])).json()
+        return render(request, 'website/blog.html', data)
 
 
 class ImageGalleryView(View):
@@ -58,9 +81,6 @@ class EventView(View):
 
 class NoticesView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'website/notices.html')
-
-
-class NoticeView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'website/notice.html')
+        data = requests.get(
+            url+"notices").json()
+        return render(request, 'website/notices.html', data)
