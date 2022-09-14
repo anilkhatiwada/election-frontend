e9f5c9ce31649abb19170f7a7b0fd75c79c5a455
Anil Khatiwada <anilkhatiwada@gmail.com>
2022-09-14 22:04:25 +0545

    code changed

--- src/website/views.py
+++ src/website/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import View
+import requests
+import json
 
 
 class HomePageView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'website/index.html')
+        data = requests.get(
+            'https://f058-103-163-182-45.in.ngrok.io/api/1/home').json()
+        return render(request, 'website/index.html', data)
 
 
 class ImageGalleryView(View):
